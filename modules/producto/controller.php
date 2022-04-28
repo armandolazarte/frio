5ed62f73adc237ca29b7d7bd865577b698a58414
Federico Vergara <hu.ce.ro@gmail.com>
2022-04-28 20:12:38 -0300

    Agregando modificación de precio producto por lote

--- modules/producto/controller.php
+++ modules/producto/controller.php
@@ -30,7 +30,7 @@ class ProductoController {
 	}
 
 	function modificacion_precio_por_lote() {
-		$select = "p.producto_id AS PRODUCTO_ID, p.codigo AS CODIGO, pc.denominacion AS CATEGORIA, CONCAT(pm.denominacion, ' ', p.denominacion) AS DENOMINACION, ROUND(p.costo, 2) as COSTO, p.iva AS IVA, p.precio_venta AS VENTA, p.ubicacion AS UBICACION";
+		$select = "p.producto_id AS PRODUCTO_ID, p.codigo AS CODIGO, CONCAT(pm.denominacion, ' ', p.denominacion) AS DENOMINACION, ROUND(p.costo, 2) as COSTO, p.flete AS FLETE, p.iva AS IVA, p.ganancia AS GANANCIA, p.precio_venta AS VENTA";
 		$from = "producto p INNER JOIN productocategoria pc ON p.productocategoria = pc.productocategoria_id INNER JOIN productomarca pm ON p.productomarca = pm.productomarca_id INNER JOIN productounidad pu ON p.productounidad = pu.productounidad_id";
 		$where = "p.oculto = 0";
 		$producto_collection = CollectorCondition()->get('Producto', $where, 4, $from, $select);
