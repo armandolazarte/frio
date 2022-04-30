b5237b83f0f526d3188f0b0044eb7f92130849d2
Luis Federico Vergara <hu.ce.ro@gmail.com>
2022-04-29 22:26:44 -0300

    Probando listado para modificar precios por lote

--- modules/producto/controller.php
+++ modules/producto/controller.php
@@ -30,7 +30,7 @@ class ProductoController {
 	}
 
 	function modificacion_precio_por_lote() {
-		$select = "p.producto_id AS PRODUCTO_ID, p.codigo AS CODIGO, CONCAT(pm.denominacion, ' ', p.denominacion) AS DENOMINACION, ROUND(p.costo, 2) as COSTO, p.flete AS FLETE, p.iva AS IVA, p.porcentaje_ganancia AS GANANCIA, p.precio_venta AS VENTA";
+		$select = "p.producto_id AS PROID, p.codigo AS CODIGO, CONCAT(pm.denominacion, ' ', p.denominacion) AS DENOMINACION, ROUND(p.costo, 2) as COSTO, p.flete AS FLETE, p.iva AS IVA, p.porcentaje_ganancia AS GANANCIA, p.precio_venta AS VENTA";
 		$from = "producto p INNER JOIN productocategoria pc ON p.productocategoria = pc.productocategoria_id INNER JOIN productomarca pm ON p.productomarca = pm.productomarca_id INNER JOIN productounidad pu ON p.productounidad = pu.productounidad_id";
 		$where = "p.oculto = 0";
 		$producto_collection = CollectorCondition()->get('Producto', $where, 4, $from, $select);
