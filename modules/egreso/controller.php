dc88c9840a7bc64e626e7be55f4f52b0d061ab4e
Luis Federico Vergara <hu.ce.ro@gmail.com>
2022-05-30 00:54:59 -0300

    Aplicando lista de precio

--- modules/egreso/controller.php
+++ modules/egreso/controller.php
@@ -798,18 +798,17 @@ class EgresoController {
 			//PRECIO VENTA
 			$pvp = $valor_neto + ($porcentaje_ganancia * $valor_neto / 100);
 			//PRECIO VENTA AL MOMENTO DE LA FACTURACIÓN CON LISTA DE PRECIO
-			$valor_por_listaprecio = $porcentaje_listaprecio * $costo_producto / 100;
-			/*
+			$valor_por_listaprecio = $porcentaje_listaprecio * $pvp / 100;
 			if ($condicion_listaprecio == '+') {
 				$pvp_factura = $pvp + $valor_por_listaprecio;						
 			} elseif ($condicion_listaprecio == '-') {
 				$pvp_factura = $pvp - $valor_por_listaprecio;
 			}
-			*/
+			
 			//IMPORTE NETO
 			$total_neto = $valor_neto * $cantidad;
 			//IMPORTE VENTA
-			$total_pvp = $pvp * $cantidad;
+			$total_pvp = $pvp_factura * $cantidad;
 
 			//DESCUENTO
 			$valor_descuento_recalculado = $descuento * $total_pvp / 100;
@@ -830,7 +829,7 @@ class EgresoController {
 			$edm->valor_descuento = round($valor_descuento_recalculado, 2);
 			$edm->descuento = $descuento;
 			$edm->neto_producto = $neto;
-			$edm->costo_producto = round($pvp, 2);
+			$edm->costo_producto = round($pvp_factura, 2);
 			$edm->iva = $iva;
 			$edm->importe = $importe_final;
 			$edm->valor_ganancia = $ganancia_final;
