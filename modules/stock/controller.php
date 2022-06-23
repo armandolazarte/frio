101d82edb24afbf8a2d78a0960feb390ca926795
Federico Vergara <hu.ce.ro@gmail.com>
2022-06-23 19:38:17 +0200

    Arreglando editar producto

--- modules/stock/controller.php
+++ modules/stock/controller.php
@@ -687,7 +687,7 @@ class StockController {
 		
 		$select = "s.stock_id AS ID, date_format(s.fecha, '%d/%m/%Y') AS FECMOD, s.fecha AS FECHA, s.hora AS HORA, CONCAT(s.cantidad_actual, pu.denominacion) AS CANT, s.concepto AS CONCEPTO, CONCAT(s.cantidad_movimiento, pu.denominacion) AS MOVIMIENTO";
 		$from = "stock s INNER JOIN producto p ON s.producto_id = p.producto_id INNER JOIN productounidad pu  ON p.productounidad = pu.productounidad_id";
-		$where = "s.producto_id = {$arg} ORDER BY s.fecha DESC";
+		$where = "s.producto_id = {$arg} ORDER BY s.fecha DESC, s.hora DESC";
 		$stock_collection = CollectorCondition()->get('Stock', $where, 4, $from, $select);
 
 		if ($stock_collection == 0 || empty($stock_collection) || !is_array($stock_collection)) {
