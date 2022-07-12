5bf8eee883fbaa8cc82dbb7e6421dc1af9eda134
Federico Vergara <hu.ce.ro@gmail.com>
2022-07-12 21:53:06 +0200

    Generando reporte de cantidades de ventas de producto por marca y fecha

--- modules/reporte/controller.php
+++ modules/reporte/controller.php
@@ -3183,6 +3183,9 @@ class ReporteController {
 		$desde = filter_input(INPUT_POST, 'desde');
 		$hasta = filter_input(INPUT_POST, 'hasta');
 		$productomarca = filter_input(INPUT_POST, 'productomarca');
+		$desde = '2022-07-01';
+		$hasta = '2022-07-12';
+		$productomarca = 1;
 
 		$select = "ed.egreso_id AS EGRID";
 		$from = "egresodetalle ed INNER JOIN egreso e ON ed.egreso_id = e.egreso_id INNER JOIN producto p ON ed.producto_id = p.producto_id";
