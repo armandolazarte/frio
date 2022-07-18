da83f303e612d72a862840fcd33cf42e01e8ebdb
Federico Vergara <hu.ce.ro@gmail.com>
2022-07-18 19:52:19 +0200

    Agregando pago conjunto con cheque

--- modules/cuentacorrientecliente/controller.php
+++ modules/cuentacorrientecliente/controller.php
@@ -221,7 +221,7 @@ class CuentaCorrienteClienteController {
 			$new_balance = ($balance >= 0) ? "$" . $balance : str_replace('-', '-$', $balance);
 			
 			$cuentacorriente_collection[$clave]['BALANCE'] = $new_balance;
-			$cuentacorriente_collection[$clave]['BALCAL'] = $balance;
+			$cuentacorriente_collection[$clave]['BALCAL'] = abs($balance);
 			$cuentacorriente_collection[$clave]['BCOLOR'] = $balance_class;
 			$cuentacorriente_collection[$clave]['BTN_DISPLAY'] = $array_temp[0]['BTN_DISPLAY'];
 			
