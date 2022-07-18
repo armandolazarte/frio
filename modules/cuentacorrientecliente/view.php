b3aa75d50babd8e09bc029dba9862cbe31bfc452
Federico Vergara <hu.ce.ro@gmail.com>
2022-07-18 19:16:20 +0200

    Agregando posibilidad de editar la fecha de cobranza

--- modules/cuentacorrientecliente/view.php
+++ modules/cuentacorrientecliente/view.php
@@ -166,6 +166,63 @@ class CuentaCorrienteClienteView extends View {
 		print $template;
 	}
 
+	function desa_consultar($cuentascorrientes_collection, $cuentacorriente_collection, $montos_cuentacorriente, $obj_cliente) {
+		$gui = file_get_contents("static/modules/cuentacorrientecliente/desa_consultar.html");
+		$gui_lst_infocontacto = file_get_contents("static/common/lst_infocontacto.html");
+		$tbl_cuentascorrientes_array = file_get_contents("static/modules/cuentacorrientecliente/tbl_cuentacorriente_array.html");
+		$gui_tbl_cuentacorriente = file_get_contents("static/modules/cuentacorrientecliente/tbl_cuentacorriente_expandido_array.html");
+
+		foreach ($cuentascorrientes_collection as $clave=>$valor) {
+			$deuda = (is_null($valor['DEUDA'])) ? 0 : round($valor['DEUDA'],2);
+			$ingreso = (is_null($valor['INGRESO'])) ? 0 : round($valor['INGRESO'],2);
+			$cuenta = round(($ingreso - $deuda),2);
+			$cuenta = ($cuenta > 0 AND $cuenta < 1) ? 0 : $cuenta;
+			$cuenta = ($cuenta > -1 AND $cuenta < 0) ? 0 : $cuenta;
+			$class = ($cuenta >= 0) ? 'info' : 'danger';
+			$cuentascorrientes_collection[$clave]['CUENTA'] = abs($cuenta);
+			$cuentascorrientes_collection[$clave]['CLASS'] = $class;
+		}
+		
+		$obj_cliente->codigo = str_pad($obj_cliente->cliente_id, 5, '0', STR_PAD_LEFT);
+		if ($obj_cliente->documentotipo->denominacion == 'CUIL' OR $obj_cliente->documentotipo->denominacion == 'CUIT') {
+			$cuil1 = substr($obj_cliente->documento, 0, 2);
+			$cuil2 = substr($obj_cliente->documento, 2, 8);
+			$cuil3 = substr($obj_cliente->documento, 10);
+			$obj_cliente->documento = "{$cuil1}-{$cuil2}-{$cuil3}";
+		}
+
+		$infocontacto_collection = $obj_cliente->infocontacto_collection;
+		unset($obj_cliente->infocontacto_collection, $obj_cliente->vendedor->infocontacto_collection, 
+			  $obj_cliente->vendedor->frecuenciaventa, $obj_cliente->flete->infocontacto_collection);	
+		$obj_cliente = $this->set_dict($obj_cliente);
+
+		$deuda = (is_null($montos_cuentacorriente[0]['DEUDA'])) ? 0 : $montos_cuentacorriente[0]['DEUDA'];
+		$ingreso = (is_null($montos_cuentacorriente[0]['INGRESO'])) ? 0 : $montos_cuentacorriente[0]['INGRESO'];
+		$valor_cuentacorriente = round(($ingreso - $deuda), 2);
+		$valor_cuentacorriente = (abs($valor_cuentacorriente) > 0 AND abs($valor_cuentacorriente) < 0.99) ? 0 : $valor_cuentacorriente;
+		$class = ($valor_cuentacorriente >= 0) ? 'blue' : 'red';
+		$icon = ($valor_cuentacorriente >= 0) ? 'up' : 'down';
+		$msj = ($valor_cuentacorriente >= 0) ? 'no posee deuda!' : 'posee deuda!';
+		
+		$array_cuentacorriente = array('{cuentacorriente-valor}'=>abs($valor_cuentacorriente),
+									   '{cuentacorriente-icon}'=>$icon,
+									   '{cuentacorriente-msj}'=>$msj,
+									   '{cuentacorriente-class}'=>$class);
+
+		$gui_lst_infocontacto = $this->render_regex('LST_INFOCONTACTO', $gui_lst_infocontacto, $infocontacto_collection);
+		$gui_tbl_cuentacorriente = $this->render_regex_dict('TBL_CUENTACORRIENTE', $gui_tbl_cuentacorriente, $cuentacorriente_collection);
+		$tbl_cuentascorrientes_array = $this->render_regex_dict('TBL_CUENTACORRIENTE', $tbl_cuentascorrientes_array, $cuentascorrientes_collection);
+		$render = str_replace('{lst_infocontacto}', $gui_lst_infocontacto, $gui);
+		$render = str_replace('{tbl_cuentacorriente}', $gui_tbl_cuentacorriente, $render);
+		$render = str_replace('{tbl_cuentascorrientes}', $tbl_cuentascorrientes_array, $render);
+		$render = str_replace('{anio_actual}', date('Y'), $render);
+		$render = $this->render($obj_cliente, $render);
+		$render = $this->render($array_cuentacorriente, $render);
+		$render = $this->render_breadcrumb($render);
+		$template = $this->render_template($render);
+		print $template;
+	}
+
 	function vdr_consultar($cuentacorriente_collection, $montos_cuentacorriente, $obj_cliente) {
 		$gui = file_get_contents("static/modules/cuentacorrientecliente/vdr_consultar.html");
 		$gui_lst_infocontacto = file_get_contents("static/common/lst_infocontacto.html");
