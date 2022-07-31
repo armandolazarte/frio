d0f0edc32834bfcdd924545dcbbab2d79b0c5e33
Federico Vergara <hu.ce.ro@gmail.com>
2022-07-31 14:34:27 +0200

    Subiendo consultar pagos con cheque por parte de un cliente

--- modules/cuentacorrienteclientecredito/controller.php
+++ modules/cuentacorrienteclientecredito/controller.php
@@ -9,5 +9,21 @@ class CuentaCorrienteClienteCreditoController {
 		$this->model = new CuentaCorrienteClienteCredito();
 		$this->view = new CuentaCorrienteClienteCreditoView();
 	}
+
+	function consultar_pagos_cliente($arg) {
+		SessionHandler()->check_session();
+		$cliente_id = $arg;
+
+		$cm = new Cliente();
+		$cm->cliente_id = $cliente_id;
+		$cm->get();
+
+		$select = "cccc.cuentacorrienteclientecredito_id AS CTACTECLICREID, cccc.fecha AS FECHA, cccc.referencia AS REFERENCIA, cccc.importe AS BALANCE, cccc.movimiento AS MOVIMIENTO";
+		$from = "cuentacorrienteclientecredito cccc";
+		$where = "cccc.cliente_id = {$cliente_id}";
+		$credito_collection = CollectorCondition()->get('CuentaCorrienteClienteCredito', $where, 4, $from, $select);
+
+		$this->view->consultar_pagos_cliente($credito_collection, $cm);
+	}
 }
 ?>
--- modules/cuentacorrienteclientecredito/view.php
+++ modules/cuentacorrienteclientecredito/view.php
@@ -1,5 +1,23 @@
 <?php
 
 
-class CuentaCorrienteClienteCreditoView extends View {}
+class CuentaCorrienteClienteCreditoView extends View {
+	function consultar_pagos_cliente($credito_collection, $obj_cliente) {
+		$gui = file_get_contents("static/modules/cuentacorrienteclientecredito/consultar_pagos_cliente.html");
+		$tbl_credito = file_get_contents("static/modules/cuentacorrienteclientecredito/tbl_credito.html");
+		$tbl_credito = $this->render_regex_dict('TBL_CUENTACORRIENTECLIENTECREDITO', $tbl_credito, $credito_collection);
+
+		$obj_vendedor = $obj_cliente->vendedor;
+		$obj_flete = $obj_cliente->flete;
+		$infocontacto_collection = $obj_cliente->infocontacto_collection;
+		unset($obj_cliente->vendedor, $obj_cliente->flete, $obj_cliente->infocontacto_collection);
+		$obj_cliente = $this->set_dict($obj_cliente);
+
+		$render = str_replace('{tbl_credito}', $tbl_credito, $gui);
+		$render = $this->render($obj_cliente, $render);
+		$render = $this->render_breadcrumb($render);
+		$template = $this->render_template($render);
+		print $template;
+	}
+}
 ?>
