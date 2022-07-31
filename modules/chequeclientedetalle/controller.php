917197b07a31d3c9af1bb880820f9e3319f4c96a
Federico Vergara <hu.ce.ro@gmail.com>
2022-07-31 13:53:05 +0200

    Subiendo consultar pagos con cheque por parte de un cliente

--- modules/chequeclientedetalle/controller.php
+++ modules/chequeclientedetalle/controller.php
@@ -22,7 +22,7 @@ class ChequeClienteDetalleController {
 		$select = "ccd.chequeclientedetalle_id AS CHECLIDETID, ccd.fecha_pago AS FECHA, ccd.numero AS NUM_CHEQUE, ccd.titular AS TITULAR, ccd.banco AS BANCO, CASE WHEN eafip.egresoafip_id IS NULL THEN CONCAT((SELECT tf.nomenclatura FROM tipofactura tf WHERE e.tipofactura = tf.tipofactura_id), ' ', LPAD(e.punto_venta, 4, 0), '-', LPAD(e.numero_factura, 8, 0)) ELSE CONCAT((SELECT tf.nomenclatura FROM tipofactura tf WHERE eafip.tipofactura = tf.tipofactura_id), ' ', LPAD(eafip.punto_venta, 4, 0), '-', LPAD(eafip.numero_factura, 8, 0)) END AS FACTURA, ccc.cuentacorrientecliente_id AS MOVCCC, ccc.ingreso AS PAGO";
 		$from = "chequeclientedetalle ccd INNER JOIN cuentacorrientecliente ccc ON ccd.cuentacorrientecliente_id = ccc.cuentacorrientecliente_id INNER JOIN egreso e ON ccc.egreso_id = e.egreso_id LEFT JOIN egresoafip eafip ON e.egreso_id = eafip.egreso_id";
 		$where = "ccc.cliente_id = {$cliente_id}";
-		$chequeclientedetalle_collection = CollectorCondition()->get('ChequeClienteDetalle', $where, 4, $from, $select);
+		$pagos_chequeclientedetalle_collection = CollectorCondition()->get('ChequeClienteDetalle', $where, 4, $from, $select);
 
 		$select = "ccd.fecha_pago AS FECHA, ccd.numero AS CHEQUE, ROUND((SUM(ccc.ingreso)), 2) AS PAGO";
 		$from = "chequeclientedetalle ccd INNER JOIN cuentacorrientecliente ccc ON ccd.cuentacorrientecliente_id = ccc.cuentacorrientecliente_id";
@@ -30,7 +30,7 @@ class ChequeClienteDetalleController {
 		$groupby = "ccd.numero";
 		$detallecheque_collection = CollectorCondition()->get('ChequeClienteDetalle', $where, 4, $from, $select, $groupby);
 
-		foreach ($detallecheque_collection as $clave=>$valor) {
+		foreach ($chequeclientedetalle_collection as $clave=>$valor) {
 			$num_cheque = $valor['CHEQUE'];
 			$select = "cccc.chequeclientedetalle_id AS CHECLIDETID, ccd.numero AS CHEQUE, cccc.movimiento SOBRANTE";
 			$from = "cuentacorrienteclientecredito cccc INNER JOIN chequeclientedetalle ccd ON cccc.chequeclientedetalle_id = ccd.chequeclientedetalle_id";
@@ -38,19 +38,15 @@ class ChequeClienteDetalleController {
 			$sobrantecheque = CollectorCondition()->get('CuentaCorrienteClienteCredito', $where, 4, $from, $select);
 
 			if (is_array($sobrantecheque) AND !empty($sobrantecheque)) {
-				$detallecheque_collection[$clave]['CREDITO'] = $sobrantecheque[0]['SOBRANTE'];
-				$detallecheque_collection[$clave]['TOTAL'] = round(($valor['PAGO'] + $sobrantecheque[0]['SOBRANTE']) , 2);
+				$chequeclientedetalle_collection[$clave]['CREDITO'] = $sobrantecheque[0]['SOBRANTE'];
+				$chequeclientedetalle_collection[$clave]['TOTAL'] = round(($valor['PAGO'] + $sobrantecheque[0]['SOBRANTE']) , 2);
 			} else {
-				$detallecheque_collection[$clave]['CREDITO'] = 0;
-				$detallecheque_collection[$clave]['TOTAL'] = round($valor['PAGO'], 2);
+				$chequeclientedetalle_collection[$clave]['CREDITO'] = 0;
+				$chequeclientedetalle_collection[$clave]['TOTAL'] = round($valor['PAGO'], 2);
 			}
 		}
 
-		print_r($detallecheque_collection);
-		exit;
-
-
-		$this->view->consultar_pagos_cliente($chequeclientedetalle_collection, $cm);
+		$this->view->consultar_pagos_cliente($pagos_chequeclientedetalle_collection, $chequeclientedetalle_collection, $cm);
 	}
 }
 ?>
--- modules/chequeclientedetalle/view.php
+++ modules/chequeclientedetalle/view.php
@@ -2,10 +2,12 @@
 
 
 class ChequeClienteDetalleView extends View {
-	function consultar_pagos_cliente($chequeclientedetalle_collection, $obj_cliente) {
+	function consultar_pagos_cliente($pagos_chequeclientedetalle_collection, $chequeclientedetalle_collection, $obj_cliente) {
 		$gui = file_get_contents("static/modules/chequeclientedetalle/consultar_pagos_cliente.html");
 		$tbl_pagos_cheques_cliente = file_get_contents("static/modules/chequeclientedetalle/tbl_pagos_cheques_cliente.html");
 		$tbl_pagos_cheques_cliente = $this->render_regex_dict('TBL_CHEQUECLIENTEDETALLE', $tbl_pagos_cheques_cliente, $chequeclientedetalle_collection);		
+		$tbl_chequeclientedetalle = file_get_contents("static/modules/chequeclientedetalle/tbl_chequeclientedetalle.html");
+		$tbl_chequeclientedetalle = $this->render_regex_dict('TBL_CHEQUECLIENTEDETALLE', $tbl_chequeclientedetalle, $chequeclientedetalle_collection);		
 
 		$obj_vendedor = $obj_cliente->vendedor;
 		$obj_flete = $obj_cliente->flete;
@@ -15,6 +17,7 @@ class ChequeClienteDetalleView extends View {
 		$obj_cliente = $this->set_dict($obj_cliente);
 
 		$render = str_replace('{tbl_pagos_cheques_cliente}', $tbl_pagos_cheques_cliente, $gui);
+		$render = str_replace('{tbl_chequeclientedetalle}', $tbl_chequeclientedetalle, $render);
 		$render = $this->render($obj_cliente, $render);
 		$render = $this->render_breadcrumb($render);
 		$template = $this->render_template($render);
