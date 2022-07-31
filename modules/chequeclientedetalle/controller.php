6249629d1938136be471d7a3eea06c652b020b70
Federico Vergara <hu.ce.ro@gmail.com>
2022-07-31 12:22:20 +0200

    Subiendo consultar pagos con cheque por parte de un cliente

--- modules/chequeclientedetalle/controller.php
+++ modules/chequeclientedetalle/controller.php
@@ -9,5 +9,17 @@ class ChequeClienteDetalleController {
 		$this->model = new ChequeClienteDetalle();
 		$this->view = new ChequeClienteDetalleView();
 	}
+
+	function consultar_pagos_cliente($arg) {
+		SessionHandler()->check_session();
+		$cliente_id = $arg;
+
+		$select = "ccd.chequeclientedetalle_id AS CHECLIDETID, ccd.fecha_pago AS FECHA, ccd.numero AS NUM_CHEQUE, ccd.titular AS TITULAR, ccd.documento AS DOCUMENTO, CASE WHEN eafip.egresoafip_id IS NULL THEN CONCAT((SELECT tf.nomenclatura FROM tipofactura tf WHERE e.tipofactura = tf.tipofactura_id), ' ', LPAD(e.punto_venta, 4, 0), '-', LPAD(e.numero_factura, 8, 0)) ELSE CONCAT((SELECT tf.nomenclatura FROM tipofactura tf WHERE eafip.tipofactura = tf.tipofactura_id), ' ', LPAD(eafip.punto_venta, 4, 0), '-', LPAD(eafip.numero_factura, 8, 0)) END AS FACTURA, ccc.cuentacorrientecliente_id AS MOVCCC, ccc.ingreso AS PAGO";
+		$from = "chequeclientedetalle ccd INNER JOIN cuentacorrientecliente ccc ON ccd.cuentacorrientecliente_id = ccc.cuentacorrientecliente_id INNER JOIN egreso e ON ccc.egreso_id = e.egreso_id LEFT JOIN egresoafip eafip ON e.egreso_id = eafip.egreso_id";
+		$where = "ccc.cliente_id = {$cliente_id}";
+		$chequeclientedetalle_collection = CollectorCondition()->get('ChequeClienteDetalle', $where, 4, $from, $select);
+
+		$this->view->consultar_pagos_cliente($chequeclientedetalle_collection);
+	}
 }
 ?>
--- modules/chequeclientedetalle/view.php
+++ modules/chequeclientedetalle/view.php
@@ -1,5 +1,16 @@
 <?php
 
 
-class ChequeClienteDetalleView extends View {}
+class ChequeClienteDetalleView extends View {
+	function consultar_pagos_cliente($chequeclientedetalle_collection) {
+		$gui = file_get_contents("static/modules/chequeclientedetalle/consultar_pagos_cliente.html");
+		$tbl_pagos_cheques_cliente = file_get_contents("static/modules/chequeclientedetalle/tbl_pagos_cheques_cliente.html");
+		$tbl_pagos_cheques_cliente = $this->render_regex_dict('TBL_CHEQUECLIENTEDETALLE', $tbl_pagos_cheques_cliente, $chequeclientedetalle_collection);		
+		
+		$render = str_replace('{tbl_pagos_cheques_cliente}', $tbl_pagos_cheques_cliente, $gui);
+		$render = $this->render_breadcrumb($render);
+		$template = $this->render_template($render);
+		print $template;
+	}
+}
 ?>
