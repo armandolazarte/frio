1c2e41d223f637cbc4b05417c3fc96792e6584b6
Federico Vergara <hu.ce.ro@gmail.com>
2022-07-31 15:25:50 +0200

    Cargando consultar movimientos de credito

--- modules/cuentacorrientecliente/view.php
+++ modules/cuentacorrientecliente/view.php
@@ -220,6 +220,7 @@ class CuentaCorrienteClienteView extends View {
 		$render = str_replace('{tbl_cuentascorrientes}', $tbl_cuentascorrientes_array, $render);
 		$render = str_replace('{slt_cobrador}', $gui_slt_cobrador, $render);
 		$render = str_replace('{anio_actual}', date('Y'), $render);
+		$render = str_replace('{fecha_sys}', date('Y-m-d'), $render);
 		$render = $this->render($obj_cliente, $render);
 		$render = $this->render($array_cuentacorriente, $render);
 		$render = $this->render_breadcrumb($render);
