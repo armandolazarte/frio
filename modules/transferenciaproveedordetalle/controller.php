91510c0b683437aed995461eb4e7b1abb310ed2c
Federico Vergara <hu.ce.ro@gmail.com>
2022-08-21 11:01:35 +0200

    Agregando credito a proveedores

--- modules/transferenciaproveedordetalle/controller.php
+++ modules/transferenciaproveedordetalle/controller.php
@@ -1,6 +1,7 @@
 <?php
 require_once "modules/transferenciaproveedordetalle/model.php";
 require_once "modules/transferenciaproveedordetalle/view.php";
+require_once "modules/proveedor/model.php";
 
 
 class TransferenciaProveedorDetalleController {
@@ -8,6 +9,44 @@ class TransferenciaProveedorDetalleController {
 	function __construct() {
 		$this->model = new TransferenciaProveedorDetalle();
 		$this->view = new TransferenciaProveedorDetalleView();
+	}
+
+	function consultar_pagos($arg) {
+		SessionHandler()->check_session();
+		$proveedor_id = $arg;
+
+		$pm = new Proveedor();
+		$pm->proveedor_id = $proveedor_id;
+		$pm->get();
+
+		$select = "tpd.transferenciaproveedordetalle_id AS TRAPRODETID, ccp.fecha AS FECHA, tpd.numero AS NUMTRA, tpd.banco AS BANCO, CONCAT((SELECT tf.nomenclatura FROM tipofactura tf WHERE i.tipofactura = tf.tipofactura_id), ' ', LPAD(i.punto_venta, 4, 0), '-', LPAD(i.numero_factura, 8, 0)) AS FACTURA, ccp.cuentacorrienteproveedor_id AS MOVCCP, ccp.ingreso AS PAGO";
+		$from = "transferenciaproveedordetalle tpd INNER JOIN cuentacorrienteproveedor ccp ON tpd.cuentacorrienteproveedor_id = ccp.cuentacorrienteproveedor_id INNER JOIN ingreso i ON ccp.ingreso_id = i.ingreso_id";
+		$where = "ccp.proveedor_id = {$proveedor_id}";
+		$pagos_transferenciaproveedordetalle_collection = CollectorCondition()->get('TransferenciaProveedorDetalle', $where, 4, $from, $select);
+
+		$select = "ccp.fecha AS FECHA, tpd.numero AS TRANSFERENCIA, ROUND((SUM(ccp.ingreso)), 2) AS PAGO";
+		$from = "transferenciaproveedordetalle tpd INNER JOIN cuentacorrienteproveedor ccp ON tpd.cuentacorrienteproveedor_id = ccp.cuentacorrienteproveedor_id";
+		$where = "ccp.proveedor_id = {$proveedor_id}";
+		$groupby = "tpd.numero";
+		$transferenciaproveedordetalle_collection = CollectorCondition()->get('TransferenciaProveedorDetalle', $where, 4, $from, $select, $groupby);
+
+		foreach ($transferenciaproveedordetalle_collection as $clave=>$valor) {
+			$num_transferencia = $valor['TRANSFERENCIA'];
+			$select = "ccpc.transferenciaproveedordetalle_id AS TRAPRODETID, tpd.numero AS TRANSFERENCIA, ccpc.movimiento SOBRANTE";
+			$from = "cuentacorrienteproveedorcredito ccpc INNER JOIN trasnferenciaproveedordetalle tpd ON ccpc.transferenciaproveedordetalle_id = tpd.transferenciaproveedordetalle_id";
+			$where = "tpd.numero = {$num_transferencia}";
+			$sobrantetransferencia = CollectorCondition()->get('CuentaCorrienteProveedorCredito', $where, 4, $from, $select);
+
+			if (is_array($sobrantetransferencia) AND !empty($sobrantetransferencia)) {
+				$transferenciaproveedordetalle_collection[$clave]['CREDITO'] = $sobrantetransferencia[0]['SOBRANTE'];
+				$transferenciaproveedordetalle_collection[$clave]['TOTAL'] = round(($valor['PAGO'] + $sobrantetransferencia[0]['SOBRANTE']) , 2);
+			} else {
+				$transferenciaproveedordetalle_collection[$clave]['CREDITO'] = 0;
+				$transferenciaproveedordetalle_collection[$clave]['TOTAL'] = round($valor['PAGO'], 2);
+			}
+		}
+
+		$this->view->consultar_pagos($pagos_transferenciaproveedordetalle_collection, $transferenciaproveedordetalle_collection, $pm);
 	}
 }
 ?>
--- modules/transferenciaproveedordetalle/view.php
+++ modules/transferenciaproveedordetalle/view.php
@@ -1,5 +1,26 @@
 <?php
 
 
-class TransferenciaProveedorDetalleView extends View {}
+class TransferenciaProveedorDetalleView extends View {
+	function consultar_pagos($pagos_transferenciaproveedordetalle_collection, $transferenciaproveedordetalle_collection, $obj_proveedor) {
+		$gui = file_get_contents("static/modules/transferenciaproveedordetalle/consultar_pagos.html");
+		
+		$tbl_pagos_transferencias_proveedor = file_get_contents("static/modules/transferenciaproveedordetalle/tbl_pagos_transferencias_proveedor.html");
+		$tbl_pagos_transferencias_proveedor = $this->render_regex_dict('TBL_TRANSFERENCIAPROVEEDORDETALLE', $tbl_pagos_transferencias_proveedor, $pagos_transferenciaproveedordetalle_collection);
+
+		$tbl_transferenciaproveedordetalle = file_get_contents("static/modules/transferenciaproveedordetalle/tbl_transferenciaproveedordetalle.html");
+		$tbl_transferenciaproveedordetalle = $this->render_regex_dict('TBL_TRANSFERENCIAPROVEEDORDETALLE', $tbl_transferenciaproveedordetalle, $transferenciaproveedordetalle_collection);		
+
+		$infocontacto_collection = $obj_proveedor->infocontacto_collection;
+		unset($obj_proveedor->infocontacto_collection);
+		$obj_proveedor = $this->set_dict($obj_proveedor);
+
+		$render = str_replace('{tbl_pagos_transferencias_proveedor}', $tbl_pagos_transferencias_proveedor, $gui);
+		$render = str_replace('{tbl_transferenciaproveedordetalle}', $tbl_transferenciaproveedordetalle, $render);
+		$render = $this->render($obj_proveedor, $render);
+		$render = $this->render_breadcrumb($render);
+		$template = $this->render_template($render);
+		print $template;
+	}
+}
 ?>
