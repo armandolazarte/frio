fca6e7dda67196d886d9da66f759c3b78c794204
Federico Vergara <hu.ce.ro@gmail.com>
2022-08-21 22:14:52 +0200

    Agregando movimiento de caja

--- modules/movimientocaja/view.php
+++ modules/movimientocaja/view.php
@@ -7,9 +7,9 @@ class MovimientoCajaView extends View {
 		$tbl_movimientocaja = file_get_contents("static/modules/movimientocaja/tbl_movimientocaja.html");
 		$slt_movimientocajatipo = file_get_contents("static/modules/movimientocaja/slt_movimientocajatipo.html");
 
-		$tbl_movimientocaja = $this->render_regex('TBL_MOVIMIENTOCAJA', $slt_movimientocaja, $movimientocaja_collection);
+		$tbl_movimientocaja = $this->render_regex_dict('TBL_MOVIMIENTOCAJA', $slt_movimientocaja, $movimientocaja_collection);
 		$slt_movimientocajatipo = $this->render_regex('SLT_MOVIMIENTOCAJATIPO', $slt_movimientocajatipo, $movimientocajatipo_collection);		
-		
+
 		$render = str_replace('{tbl_movimientocaja}', $tbl_movimientocaja, $gui);
 		$render = str_replace('{slt_movimientocajatipo}', $slt_movimientocajatipo, $render);
 		$render = $this->render_breadcrumb($render);
