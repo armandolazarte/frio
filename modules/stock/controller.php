953fd4aa0560cc70a459e9acbc3395d8b5d6119c
Federico Vergara <hu.ce.ro@gmail.com>
2022-09-02 21:20:09 +0200

    Agregando puntos y comas a valores numéricos

--- modules/stock/controller.php
+++ modules/stock/controller.php
@@ -159,13 +159,13 @@ class StockController {
 		$array_totales = array('{estado_actual}'=>$array_totales,
 							   '{total_facturado}'=>"$" . $cajadiaria,
 							   '{total_facturado_class}'=>$total_facturado_class,
-							   '{deuda_cuentacorrientecliente}'=>$deuda_cuentacorrientecliente,
-							   '{deuda_cuentacorrienteproveedor}'=>$deuda_cuentacorrienteproveedor,
-							   '{stock_valorizado}'=>$stock_valorizado,
-							   '{ingreso_cuentacorrientecliente_hoy}'=>$ingreso_cuentacorriente_hoy,
-							   '{egreso_cuentacorrienteproveedor_hoy}'=>$egreso_cuentacorrienteproveedor_hoy,
-							   '{ingreso_contado_hoy}'=>$sum_contado,
-							   '{egreso_comision_hoy}'=>$egreso_comision_hoy);
+							   '{deuda_cuentacorrientecliente}'=>number_format($deuda_cuentacorrientecliente, 2, ',', '.'),
+							   '{deuda_cuentacorrienteproveedor}'=>number_format($deuda_cuentacorrienteproveedor, 2, ',', '.'),
+							   '{stock_valorizado}'=>number_format($stock_valorizado, 2, ',', '.'),
+							   '{ingreso_cuentacorrientecliente_hoy}'=>number_format($ingreso_cuentacorriente_hoy, 2, ',', '.'),
+							   '{egreso_cuentacorrienteproveedor_hoy}'=>number_format($egreso_cuentacorrienteproveedor_hoy, 2, ',', '.'),
+							   '{ingreso_contado_hoy}'=>number_format($sum_contado, 2, ',', '.'),
+							   '{egreso_comision_hoy}'=>number_format($egreso_comision_hoy, 2, ',', '.'));
 		
 		$this->view->panel($stock_collection, $almacen_collection, $array_totales, $am);
 	}
