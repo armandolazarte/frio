092fa9aaacf08192d284cc193cfbe348958c901b
Federico Vergara <hu.ce.ro@gmail.com>
2022-09-02 22:45:56 +0200

    Agregando puntos y comas a valores numéricos

--- modules/egreso/view.php
+++ modules/egreso/view.php
@@ -355,7 +355,7 @@ class EgresoView extends View {
 			}
 
 			$importe_iva = round(($subtotal * 0.21), 2);
-	        $obj_egreso->importe_iva = round(($subtotal * 0.21), 2);
+	        $obj_egreso->importe_iva = number_format($importe_iva, 2, ',', '.');
 	        $obj_egreso->subtotal = round($subtotal, 2);
 	        $obj_egreso->importe_total = round(($importe_iva + $subtotal), 2);
 		}
