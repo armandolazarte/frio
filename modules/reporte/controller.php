78623f271506602d19483ce5acd41c4e276b6c60
Federico Vergara <hu.ce.ro@gmail.com>
2022-09-02 20:55:25 +0200

    Agregando puntos y comas a valores numéricos

--- modules/reporte/controller.php
+++ modules/reporte/controller.php
@@ -331,8 +331,8 @@ class ReporteController {
 					$nuevo_valor_cantidad = 0;
 				}
 
-				$sum_importe_producto[$clave]['IMPORTE'] = number_format($nuevo_valor_importe, 2, ',', '.');
-				$sum_importe_producto[$clave]['CANTIDAD'] = number_format($nuevo_valor_cantidad, 2, ',', '.');
+				$sum_importe_producto[$clave]['IMPORTE'] = round($nuevo_valor_importe, 2);
+				$sum_importe_producto[$clave]['CANTIDAD'] = round($nuevo_valor_cantidad, 2);
 			}
 		}
 
