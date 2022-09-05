f68681b1c8ed696cb195107f985af15ef40a345e
Federico Vergara <hu.ce.ro@gmail.com>
2022-09-05 17:48:53 +0200

    Agregando puntos y comas a valores numéricos

--- modules/gasto/controller.php
+++ modules/gasto/controller.php
@@ -77,7 +77,7 @@ class GastoController {
 		$where = "g.fecha BETWEEN '{$desde}' AND '{$hasta}'";
 		$gasto_collection = CollectorCondition()->get('Gasto', $where, 4, $from, $select);
 
-		$select = "ROUND(SUM(g.total),2) AS IMPORTE";
+		$select = "FORMAT(SUM(g.total), 2,'de_DE') AS IMPORTE";
 		$from = "gasto g";
 		$where = "g.fecha BETWEEN '{$desde}' AND '{$hasta}'";
 		$sum_gasto = CollectorCondition()->get('Gasto', $where, 4, $from, $select);
